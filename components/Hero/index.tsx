39fe6565495f923be6bc71be5f7187ba5282489d
terabucket <stealthathos@gmail.com>
2025-07-11 04:47:40 -0500

    installed motion animation library & completed hero section

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -4,40 +4,34 @@ import { useEffect, useState } from 'react';
 import Image from "next/image";
 
 const Hero = () => {
-  const [showLight, setShowLight] = useState(false);
-
-  useEffect(() => {
-    const sequence = async () => {
-      await delay(1000);
-      setShowLight(true);
-
-      await delay(200);
-      setShowLight(false);
-
-      await delay(50);
-      setShowLight(true);
-    };
-
-    sequence();
-  }, []);
-
-  const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
-
   return (
     <>
       <section
         id="home"
         className="dark:bg-gray-dark relative z-10 overflow-hidden bg-white pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
       >
-        <div className="container">
-          <div className="w-[45rem]">
+        <div className="container flex gap-4">
+          <div className="w-[45rem] relative">
+            <div className="absolute inset-0 h-full w-full">
+              <Image
+                src="/images/hero/city-off.png"
+                alt="City off"
+                width={720}
+                height={563}
+                className="w-full"
+              />
+            </div>
             <Image
-              src={showLight ? "/images/hero/city-on.png" : "/images/hero/city-off.png"}
-              alt="City on/off"
+              src="/images/hero/city-on.png"
+              alt="City off"
               width={720}
               height={563}
-              className="w-full"
+              className="w-full relative"
             />
+          </div>
+          <div className="flex-1">
+            <h3 className="text-7xl mt-5">Empower Your Plant with AI-Driven Energy Insights</h3>
+            <p className="text-lg mt-9">Transform how you manage energy flows with real-time simulations and interactive dashboards.</p>
           </div>
         </div>
       </section>
