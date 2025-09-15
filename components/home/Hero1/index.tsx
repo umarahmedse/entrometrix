2e931aedb044ae329f6936443970bd9d716266e2
mahnoortufaile <mahnoortufailse@gmail.com>
2025-09-15 20:06:56 +0500

    fix

--- components/home/Hero1/index.tsx
+++ components/home/Hero1/index.tsx
@@ -53,7 +53,7 @@ const Hero1 = () => {
             <div
               className=" mt-8 w-fit xs:w-fit py-2 px-4 sm:py-3 sm:px-8 text-xs sm:text-sm md:text-base font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
-              <p className="text-xs sm:text-lg md:text-lg lg:text-lg  leading-snug">
+              <p className=" leading-snug">
                 Efficiency. Sustainability. Resilience.
               </p>
             </div>
